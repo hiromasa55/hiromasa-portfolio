74cb04ceec5fff67f6920a7c20b1f6cd896c4a64
hiromasa55 <masamasap55@gmail.com>
2026-05-28 01:44:24 +0900

    add menu bar and other page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header className="p-4 bg-gray-100 flex gap-9 font-bold">
+          <Link href="/" className="transition-colors duration-300 hover:text-red-500">HOME</Link>
+          <Link href="/profile" className="transition-colors duration-300 hover:text-red-500">PROFILE</Link>
+          <Link href="/works" className="transition-colors duration-300 hover:text-red-500">WORKS</Link>
+          <Link href="/contacts" className="transition-colors duration-300 hover:text-red-500">CONTACTS</Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function profile(){
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-20">Hiromasa's Profile</h1>
+    </>
+  )
+}
